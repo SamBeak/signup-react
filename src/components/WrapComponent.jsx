56dd80f33998d978b98605f48edb06c5269012dd
Baek HyunGyu <baekhg102@gmail.com>
2023-09-18 21:28:49 +0900

    Initial init

---
+++ src/components/WrapComponent.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Component List
+import FooterComponent from './wrap/FooterComponent';
+import HeaderComponent from './wrap/HeaderComponent';
+import HomeComponent from './wrap/HomeComponent';
+
+export default function WrapComponent() {
+  return (
+    <div id="wrap">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HeaderComponent />} >
+            <Route index element={<HomeComponent />} />
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <FooterComponent />
+
+      {/* 모달 메뉴 */}
+      {/* <SidebarComponent /> */}
+    </div>
+  )
+}
