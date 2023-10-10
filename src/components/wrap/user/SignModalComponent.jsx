a8df3f89d9ae167ee736e8730a0de456bac42d37
SamBeak <whdmsspt@naver.com>
2023-10-11 08:56:28 +0900

    docs: Update SignModal
    카테고리 디자인, 색상 변경

--- src/components/wrap/user/SignModalComponent.jsx
+++ src/components/wrap/user/SignModalComponent.jsx
@@ -4,9 +4,29 @@ import SignupComponent from './SignupComponent';
 
 export default function SignModalComponent() {
     const [isSignin, setIsSignin] = React.useState(false);
+
+    // 로그인 / 회원가입 버튼 클릭 => 색상 변경 => 현재 카테고리 식별 231011_bhg
     const onClickSignModal = (e) => {
+        const signinBtn = document.querySelector('#signin__btn');
+        const signupBtn = document.querySelector('#signup__btn');
         e.target.innerText === '로그인' ? setIsSignin(true) : setIsSignin(false);
+        if(e.target.innerText === '로그인'){
+            setIsSignin(true);
+            signinBtn.classList.add('active');
+            if(signupBtn.classList.contains('active')){
+                signupBtn.classList.remove('active');
+            }
+        }
+        else{
+            setIsSignin(false);
+            signupBtn.classList.add('active');
+            if(signinBtn.classList.contains('active')){
+                signinBtn.classList.remove('active');
+            }
+        }
     }
+
+    
   return (
     <div id="signModal">
         <div className="container">
@@ -15,10 +35,10 @@ export default function SignModalComponent() {
                     <div className="sign__title">
                         <ul className="sign__list">
                             <li className="sign__item">
-                                <button className="sign__btn" type="button" onClick={onClickSignModal}>로그인</button>
+                                <button id="signin__btn" className="sign__btn" type="button" onClick={onClickSignModal}>로그인</button>
                             </li>
                             <li className="sign__item">
-                                <button className="sign__btn" type="button" onClick={onClickSignModal}>회원가입</button>
+                                <button id="signup__btn" className="sign__btn" type="button" onClick={onClickSignModal}>회원가입</button>
                             </li>
                         </ul>
                     </div>
