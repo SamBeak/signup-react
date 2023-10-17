c4cbdf120a2c0a58d1fe132480d3bea4243c39c2
SamBeak <whdmsspt@naver.com>
2023-10-18 08:41:39 +0900

    docs: Update Signupcomponent
    라벨 이동 이벤트 추가

--- src/components/wrap/user/SignModalComponent.jsx
+++ src/components/wrap/user/SignModalComponent.jsx
@@ -2,43 +2,34 @@ import React from 'react';
 import SigninComponent from './SigninComponent';
 import SignupComponent from './SignupComponent';
 
-export default function SignModalComponent() {
+export default function SignModalComponent( {setIsSignModal} ) {
     const [isSignin, setIsSignin] = React.useState(false);
 
     // 로그인 / 회원가입 버튼 클릭 => 색상 변경 => 현재 카테고리 식별 231011_bhg
     const onClickSignModal = (e) => {
-        const signinBtn = document.querySelector('#signin__btn');
-        const signupBtn = document.querySelector('#signup__btn');
         e.target.innerText === '로그인' ? setIsSignin(true) : setIsSignin(false);
-        if(e.target.innerText === '로그인'){
-            setIsSignin(true);
-            signinBtn.classList.add('active');
-            if(signupBtn.classList.contains('active')){
-                signupBtn.classList.remove('active');
-            }
-        }
-        else{
-            setIsSignin(false);
-            signupBtn.classList.add('active');
-            if(signinBtn.classList.contains('active')){
-                signinBtn.classList.remove('active');
-            }
-        }
-    }
+    };
 
-    
+    // 모달창 종료 231017_bhg
+    const onClickOffModal = (e) => {
+        if(e.target.id === "signModal") {
+            setIsSignModal(false);
+        }
+    };
+
+
   return (
-    <div id="signModal">
+    <div id="signModal" onClick={onClickOffModal}>
         <div className="container">
             <div className="gap">
                 <div className="title">
                     <div className="sign__title">
                         <ul className="sign__list">
                             <li className="sign__item">
-                                <button id="signin__btn" className="sign__btn" type="button" onClick={onClickSignModal}>로그인</button>
+                                <button id="signin__btn" className={isSignin ? "sign__btn active" : "sign__btn"} type="button" onClick={onClickSignModal}>로그인</button>
                             </li>
                             <li className="sign__item">
-                                <button id="signup__btn" className="sign__btn" type="button" onClick={onClickSignModal}>회원가입</button>
+                                <button id="signup__btn" className={isSignin ? "sign__btn" : "sign__btn active"} type="button" onClick={onClickSignModal}>회원가입</button>
                             </li>
                         </ul>
                     </div>
--- src/components/wrap/user/SignupComponent.jsx
+++ src/components/wrap/user/SignupComponent.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 
 export default function SignupComponent() {
+    // 라벨 이동 이벤트 231017_bhg
+    const onMoveLabel = (e) => {
+        const forName = e.target.id;
+        const label = document.querySelector(`label[for=${forName}]`);
+        label.classList.add("active");
+
+        e.target.addEventListener("blur", () => {
+            if(e.target.value === ""){
+                label.classList.remove("active");
+            }
+        });
+    };
   return (
     <div id="signup">
         <div className="container">
             <div className="gap">
                 <ul className="signup__list">
-                    <li className="signup__item"><label htmlFor="userId">아이디</label><input type="text"     id="userId" /></li>
-                    <li className="signup__item"><label htmlFor="userPw">비밀번호</label><input type="password" id="userPw" /></li>
-                    <li className="signup__item"><label htmlFor="userPwc">비밀번호 확인</label><input type="password" id="userPwc" /></li>
-                    <li className="signup__item"><label htmlFor="userEmail">이메일</label><input type="email"    id="userEmail" /></li>
-                    <li className="signup__item"><label htmlFor="userName">이름</label><input type="text"     id="userName" /></li>
-                    <li className="signup__item"><label htmlFor="userHp">연락처</label><input type="text"     id="userHp" /></li>
-                    <li className="signup__item"><label htmlFor="userAddr">주소</label><input type="text"     id="userAddr" /></li>
-                    <li className="signup__item"><label htmlFor="userGender">성별</label><input type="text"     id="userGender" /></li>
-                    <li className="signup__item"><label htmlFor="userBirth">생년월일</label><input type="text"     id="userBirth" /></li>
+                    <li className="signup__item"><label htmlFor="userId">아이디</label><input type="text" id="userId" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userPw">비밀번호</label><input type="password" id="userPw" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userPwc">비밀번호 확인</label><input type="password" id="userPwc" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userEmail">이메일</label><input type="email" id="userEmail" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userName">이름</label><input type="text" id="userName" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userHp">연락처</label><input type="text" id="userHp" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userAddr">주소</label><input type="text" id="userAddr" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userGender">성별</label><input type="text" id="userGender" onChange={onMoveLabel} /></li>
+                    <li className="signup__item"><label htmlFor="userBirth">생년월일</label><input type="text" id="userBirth" onChange={onMoveLabel} /></li>
                 </ul>
+                <div className="btn__box">
+                    <button className="submit__btn">회원가입</button>
+                </div>
             </div>
         </div>
     </div>
